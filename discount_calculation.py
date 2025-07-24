5190252fccc7371259f303f2b3d44d70b7cfc968
SekharChawdhary <sekharabboori@gmail.com>
2025-07-24 15:44:06 +0530

    .

--- discount_calculation.py
+++ discount_calculation.py
@@ -1,41 +1,44 @@
 student_name=input("Enter Your Name: ")
 student_grade=int(input("Enter Your Garde(1-12): "))
 base_tution_fee=float(input("Enter Your Tution fee: "))
-academic_topper_states=input("Enter academic sates (Yes/No)")
+academic_topper_states=input("Are You Topper (Yes/No)")
 discount=0
-if student_grade<=1 or student_grade>12:
-        print("Invalid your Garde should be (1-12):")
+if student_grade<1 or student_grade>12:
+        print("Invalid  (Garde should be (1-12):")
         exit()
 match student_grade:
     # For grade 10
   
     case 10:
         if student_grade==10:
-            discount=base_tution_fee*0.03
-        print(f"Your name is: {student_name}")
-        print(f"Your grade is: {student_grade}")
-        print(f"Your acdemic states: {academic_topper_states}")
-        print(f"Your Tution fee is: {base_tution_fee}")
-        print(f"Student discounted amount is: {discount}")
-        print(f"Your final tution fee is{base_tution_fee-discount}")
+            percent=3
+            discount=base_tution_fee*(percent/100)
+        print(f"Name : {student_name}")
+        print(f"Grade : {student_grade}")
+        print(f"Topper Status: {academic_topper_states}")
+        print(f"Tution Fees: {base_tution_fee}")
+        print(f"Discounted: {discount} (({percent})%) Applied")
+        print(f"Final Tution Fees{base_tution_fee-discount}")
 
     # for grade 12
     case 12:
+        percent=5
         if student_grade==12:
-            discount=base_tution_fee*0.05
-        print(f"Your name is: {student_name}")
-        print(f"Your grade is: {student_grade}")
-        print(f"Your acdemic states: {academic_topper_states}")
-        print(f"Your Tution fee is: {base_tution_fee}")
-        print(f"Student discounted amount is: {discount}")
-        print(f"Your final tution fee is{base_tution_fee-discount}")
+            discount=base_tution_fee*(percent/100)
+        print(f"Name : {student_name}")
+        print(f"Grade : {student_grade}")
+        print(f"Topper Status: {academic_topper_states}")
+        print(f"Tution Fees: {base_tution_fee}")
+        print(f"Discounted: {discount}({percent}%) applied")
+        print(f"Final Tution Fees{base_tution_fee-discount}")
     # Other academics
     case _:
-        print(f"Your name is: {student_name}")
-        print(f"Your grade is: {student_grade}")
-        print(f"Your acdemic states: {academic_topper_states}")
-        print(f"Your Tution fee is: {base_tution_fee}")
-        print(f"Student discounted amount is: {discount}")
-        print(f"Your final tution fee is{base_tution_fee-discount}")
+        percent=0
+        print(f"Name : {student_name}")
+        print(f"Grade : {student_grade}")
+        print(f"Topper Status: {academic_topper_states}")
+        print(f"Tution Fees: {base_tution_fee}")
+        print(f"Discounted : {discount} ({percent}%)")
+        print(f"Final Tution Fees{base_tution_fee-discount}")
 
            
--- tution_discount.py
+++ tution_discount.py
@@ -2,25 +2,25 @@
 student_name=input("Enter Your Name: ")
 student_grade=int(input("Enter Your Garde(1-12): "))
 base_tution_fee=float(input("Enter Your Tution fee: "))
-academic_topper_states=True
+academic_topper_states=input("Are You Topper (Yes/No)")
 discount=0
 # input validation
 if student_grade<1 or student_grade>12:
     print("Invalid plese enter valid grade (1-12)")
     exit()
 #Discount calculation
-if student_grade>=9 or student_grade<=12 and academic_topper_states==True:
+if student_grade>=9 or student_grade<=12 and academic_topper_states=="Yes":
     discount=base_tution_fee*0.20
-elif student_grade>=9 or student_grade<=12 and academic_topper_states==False:
+elif student_grade>=9 or student_grade<=12 and academic_topper_states=="No":
     discount=base_tution_fee*.10
 elif student_grade>=6 or student_grade<=8:
     discount=base_tution_fee*0.5
 studen_final_fee=base_tution_fee-discount
-print(f"Your name is: {student_name}")
-print(f"Your grade is: {student_grade}")
-print(f"Your Tution fee is: {base_tution_fee}")
-print(f"Student discounted amount is: {discount}")
-print(f"Your final tution fee is:{studen_final_fee}")
+print(f"Name: {student_name}")
+print(f"Grade: {student_grade}")
+print(f"Tution Fees: {base_tution_fee}")
+print(f"Discount: {discount}")
+print(f"Final Fees:{studen_final_fee}")
 
 
 
